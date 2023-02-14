588163c4ce475218d1ea1013b1357a5ae4f817df
Eylon Goor <eylong@google.com>
2023-02-14 15:01:43 +0200

    move env var to postbuild

--- setup/prebuild.sh
+++ setup/prebuild.sh
@@ -9,6 +9,3 @@ gcloud alpha storage buckets create gs://${GOOGLE_CLOUD_PROJECT}-seatera --proje
 
 echo "Uploading config.yaml to cloud storage..."
 gcloud alpha storage cp ./config.yaml gs://${GOOGLE_CLOUD_PROJECT}-seatera
-
-echo "Setting bucket env variable.." 
-gcloud run services update seatera --update-env-vars bucket_name=${GOOGLE_CLOUD_PROJECT}-seatera --region=${GOOGLE_CLOUD_REGION}
